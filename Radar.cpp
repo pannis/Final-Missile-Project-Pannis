995568250e30f16d5905ed7763106c561494ea41
Peter Annis <pannis@mail.umw.edu>
2024-11-27 22:47:54 -0500

    put in the setters for the radar

--- Radar.cpp
+++ Radar.cpp
@@ -7,17 +7,48 @@ using namespace std;
 class Radar {
 public:
 
+	// sets the latitude of the radar
+	void setLat(int n);
+
+	// sets the longitude of the radar placed
+	void setLong(int n);
+
+	// sets the radius in yards for what the radar covers
+	void setRadius(int n);
+
+	// sets the currentDegree that teh radar is pinging
+	void setCurrent(int n);
 private:
-	
+
 	// the latitude that the radar is placed
 	int latitude;
-	
+
 	// the longitude that the radar is placed
 	int longitude;
-	
+
 	// the radius that the radar covers
 	int yardsCovered;
-	
+
 	// the current degree in the radius that is being pinged
 	int currentDegree;
+};
+
+// sets the latitude of the radar
+void Radar::setLat(int n) {
+	latitude = n;
+}
+
+// sets the longitude of the radar placed
+void Radar::setLong(int n) {
+	longitude = n;
+}
+
+// sets the radius in yards for what the radar covers
+void Radar::setRadius(int n) {
+	yardsCovered = n;
+}
+
+// sets the currentDegree that teh radar is pinging
+void Radar::setCurrent(int n) {
+	currentDegree = n;
 }
